382f943f8172e231dce24f424aa4067287e599eb
Brad Forrest <emsirikami@gmail.com>
2018-12-14 15:02:49 +1000

    Added borders to front and back deck

--- travelDeckBack/deck.rb
+++ travelDeckBack/deck.rb
@@ -5,16 +5,14 @@ layouts = ['textLayout.yml', 'iconsLayout.yml', 'layout.yml']
 
 Squib::Deck.new cards: data['name'].size, width: '2.75in', height: '4.75in', layout: layouts do
   background color: 'white'
-  rect
+  svg file: data['border'], layout: 'border'
   rect layout: 'cut'
-  rect layout: 'safe'
-  text str: data['name'], layout: 'title', font: 'Edwardian Script ITC 16', y: 70, color: '#EACD98'
+  text str: data['name'], layout: 'title', font: 'Edwardian Script ITC 16', y: 120
   text str: data['address'], layout: 'address', font: 'Arial 8'
   text str: data['description'], layout: 'description'
   svg file: './icons/info.svg', layout: 'info'
   svg file: './icons/favorite.svg', layout: 'favorite'
   svg file: './icons/help.svg', layout: 'help'
-    # png file: data['border'], layout: 'border'
   save_png
   save_pdf trim: 37.5
 end
--- travelDeckFront/deck.rb
+++ travelDeckFront/deck.rb
@@ -1,16 +1,14 @@
 require 'squib'
 
 data = Squib.csv file: 'testcsv.csv'
-layouts = ['layout.yml', 'imageLayout.yml']
+layouts = ['imageLayout.yml', 'layout.yml']
 
 Squib::Deck.new cards: data['name'].size, width: '2.75in', height: '4.75in', layout: layouts do
   background color: 'white'
   rect layout: 'cut'
-  rect layout: 'safe'
-  rect
-  text str: data['name'], layout: 'title', font: 'Edwardian Script ITC 16', y: 70, color: '#EACD98'
   png file: data['image'], layout: 'image'
-  # png file: data['border'], layout: 'border'
+  svg file: data['border'], layout: 'border'
+  text str: data['name'], layout: 'title', font: 'Edwardian Script ITC 16', y: 120, color: '#EACD98'
   save_png
   save_pdf trim: 37.5
 end
